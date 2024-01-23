be23fd00103094db89c5c63f1f29695d623aaae6
KMzKINGth <643170010325@rmu.ac.th>
2024-01-23 13:57:34 +0700

    init

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -1,29 +1,30 @@
 exports.register = (req, res, next) => {
-  const { email, password} = req.body;
+  const { email, password } = req.body;
 
-  res.json({ email, password})
-};
-
+  res.json({ email, password });
+}
 
 exports.login = (req, res, next) => {
-  res.json({ message: "Login" });
-};
+  const { email, password } = req.body
+  res.json({ email, password })
+}
 
 exports.forgetPassword = (req, res, next) => {
   const { email } = req.body
 
-  res.json({ email });
-};
+  res.json({ email })
+}
+
 
 exports.verifyForgetPassword = (req, res, next) => {
   const { token } = req.params
 
-  res.json({ token });
-};
+  res.json({ token })
+}
 
 exports.resetPassword = (req, res, next) => {
   const { token } = req.params
   const { password } = req.body
 
-  res.json({ token, password });
-};
+  res.json({ token, password })
+}
